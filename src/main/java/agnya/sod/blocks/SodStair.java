021ea8ccacd551179a35d30c9134c6732d150827
nvb-uy <almhak@proton.me>
2022-10-13 12:58:41 -0500

    + Stair block class

---
+++ src/main/java/agnya/sod/blocks/SodStair.java
@@ -0,0 +1,9 @@
+package agnya.sod.blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.StairsBlock;
+
+public class SodStair extends StairsBlock {
+    public SodStair(BlockState baseBlockState, Settings settings) {
+        super(baseBlockState, settings);
+    }
+}
