48c948c634aa2f54c5579bb29318b258a7de3da4
nvb-uy <almhak@proton.me>
2022-10-19 05:53:35 -0500

    + Dirt sod stair

--- forge/1.18.2/src/main/java/agnya/sod/init/SodItems.java
+++ forge/1.18.2/src/main/java/agnya/sod/init/SodItems.java
@@ -13,23 +13,30 @@ import agnya.sod.Sod;
 
 public class SodItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, Sod.MODID);
+	
 	public static final RegistryObject<Item> DIRT_SOD_SLAB = block(SodBlocks.DIRT_SOD_SLAB, Sod.TAB_SOD);
 	public static final RegistryObject<Item> DIRT_SOD_PATCH = block(SodBlocks.DIRT_SOD_PATCH, Sod.TAB_SOD);
+	
 	public static final RegistryObject<Item> GRASS_SOD_BLOCK = block(SodBlocks.GRASS_SOD_BLOCK, Sod.TAB_SOD);
 	public static final RegistryObject<Item> GRASS_SOD_SLAB = block(SodBlocks.GRASS_SOD_SLAB, Sod.TAB_SOD);
 	public static final RegistryObject<Item> GRASS_SOD_PATCH = block(SodBlocks.GRASS_SOD_PATCH, Sod.TAB_SOD);
+	
 	public static final RegistryObject<Item> PATH_SOD_BLOCK = block(SodBlocks.PATH_SOD_BLOCK, Sod.TAB_SOD);
 	public static final RegistryObject<Item> PATH_SOD_SLAB = block(SodBlocks.PATH_SOD_SLAB, Sod.TAB_SOD);
 	public static final RegistryObject<Item> PATH_SOD_PATCH = block(SodBlocks.PATH_SOD_PATCH, Sod.TAB_SOD);
+	
 	public static final RegistryObject<Item> MYCELIUM_SOD_BLOCK = block(SodBlocks.MYCELIUM_SOD_BLOCK, Sod.TAB_SOD);
 	public static final RegistryObject<Item> MYCELIUM_SOD_SLAB = block(SodBlocks.MYCELIUM_SOD_SLAB, Sod.TAB_SOD);
 	public static final RegistryObject<Item> MYCELIUM_SOD_PATCH = block(SodBlocks.MYCELIUM_SOD_PATCH, Sod.TAB_SOD);
+	
 	public static final RegistryObject<Item> PODZOL_SOD_BLOCK = block(SodBlocks.PODZOL_SOD_BLOCK, Sod.TAB_SOD);
 	public static final RegistryObject<Item> PODZOL_SOD_SLAB = block(SodBlocks.PODZOL_SOD_SLAB, Sod.TAB_SOD);
 	public static final RegistryObject<Item> PODZOL_SOD_PATCH = block(SodBlocks.PODZOL_SOD_PATCH, Sod.TAB_SOD);
+	
 	public static final RegistryObject<Item> CRIMSON_NYLIUM_SOD_BLOCK = block(SodBlocks.CRIMSON_NYLIUM_SOD_BLOCK, Sod.TAB_SOD);
 	public static final RegistryObject<Item> CRIMSON_NYLIUM_SOD_SLAB = block(SodBlocks.CRIMSON_NYLIUM_SOD_SLAB, Sod.TAB_SOD);
 	public static final RegistryObject<Item> CRIMSON_NYLIUM_SOD_PATCH = block(SodBlocks.CRIMSON_NYLIUM_SOD_PATCH, Sod.TAB_SOD);
+	
 	public static final RegistryObject<Item> WARPED_NYLIUM_SOD_BLOCK = block(SodBlocks.WARPED_NYLIUM_SOD_BLOCK, Sod.TAB_SOD);
 	public static final RegistryObject<Item> WARPED_NYLIUM_SOD_SLAB = block(SodBlocks.WARPED_NYLIUM_SOD_SLAB, Sod.TAB_SOD);
 	public static final RegistryObject<Item> WARPED_NYLIUM_SOD_PATCH = block(SodBlocks.WARPED_NYLIUM_SOD_PATCH, Sod.TAB_SOD);
