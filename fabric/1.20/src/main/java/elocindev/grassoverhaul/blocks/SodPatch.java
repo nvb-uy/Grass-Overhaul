bd8efe2944a38f432efb1d1c35b38413bb480f99
nvb-uy <nvb-uy@github.com>
2023-10-11 11:50:10 -0300

    feat(Snow real magic compat)

--- fabric/1.20/src/main/java/elocindev/grassoverhaul/blocks/SodPatch.java
+++ fabric/1.20/src/main/java/elocindev/grassoverhaul/blocks/SodPatch.java
@@ -1,11 +1,119 @@
 package elocindev.grassoverhaul.blocks;
 
-import net.minecraft.block.SnowBlock;
+import org.jetbrains.annotations.Nullable;
 
-public class SodPatch extends SnowBlock {
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.ai.pathing.NavigationType;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager.Builder;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+
+public class SodPatch extends Block  {
+
+    public static final int MAX_LAYERS = 8;
+    public static final IntProperty LAYERS;
+    protected static final VoxelShape[] LAYERS_TO_SHAPE;
+    public static final int field_31248 = 5;
 
     public SodPatch(Settings settings) {
         super(settings);
+        this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(LAYERS, 1));
+    }
+
+    public boolean canPathfindThrough(BlockState state, BlockView world, BlockPos pos, NavigationType type) {
+        return (Integer)state.get(LAYERS) < 5;
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return LAYERS_TO_SHAPE[(Integer)state.get(LAYERS)];
+    }
+
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return LAYERS_TO_SHAPE[(Integer)state.get(LAYERS) - 1];
+    }
+
+    public VoxelShape getSidesShape(BlockState state, BlockView world, BlockPos pos) {
+        return LAYERS_TO_SHAPE[(Integer)state.get(LAYERS)];
+    }
+
+    public VoxelShape getCameraCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return LAYERS_TO_SHAPE[(Integer)state.get(LAYERS)];
+    }
+
+    public boolean hasSidedTransparency(BlockState state) {
+        return true;
+    }
+
+    public float getAmbientOcclusionLightLevel(BlockState state, BlockView world, BlockPos pos) {
+        return (Integer)state.get(LAYERS) == 8 ? 0.2F : 1.0F;
+    }
+
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        BlockState downState = world.getBlockState(pos.down());
+        if (downState.isIn(BlockTags.SNOW_LAYER_CANNOT_SURVIVE_ON)) {
+            return false;
+        } else if (downState.isIn(BlockTags.SNOW_LAYER_CAN_SURVIVE_ON)) {
+            return true;
+        } else {
+            return Block.isFaceFullSquare(downState.getCollisionShape(world, pos.down()), Direction.UP) || downState.isOf(this) && (Integer)downState.get(LAYERS) == 8;
+        }
+    }
+
+    @Deprecated
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        return !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+
+    }
+
+    public boolean canReplace(BlockState state, ItemPlacementContext context) {
+        int i = (Integer)state.get(LAYERS);
+        if (context.getStack().isOf(this.asItem()) && i < 8) {
+            if (context.canReplaceExisting()) {
+                return context.getSide() == Direction.UP;
+            } else {
+                return true;
+            }
+        } else {
+            return i == 1;
+        }
+    }
+
+    @Nullable
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockState blockState = ctx.getWorld().getBlockState(ctx.getBlockPos());
+        if (blockState.isOf(this)) {
+            int i = (Integer)blockState.get(LAYERS);
+            return (BlockState)blockState.with(LAYERS, Math.min(8, i + 1));
+        } else {
+            return super.getPlacementState(ctx);
+        }
+    }
+
+    protected void appendProperties(Builder<Block, BlockState> builder) {
+        builder.add(new Property[]{LAYERS});
+    }
+
+    static {
+        LAYERS = Properties.LAYERS;
+        LAYERS_TO_SHAPE = new VoxelShape[]{VoxelShapes.empty(), Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D), Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D), Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D), Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D), Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D), Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D), Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D), Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D)};
     }
 
 }
